51ae65c6f3f0d4f241e0d33cd14f746c88baae0d
TDSLahiru <84264249+TDSLahiru@users.noreply.github.com>
2021-05-24 01:26:10 +0530

    Initial Commit

---
+++ shopingCartApp/src/main/java/com/sl/shopingCart/response/CommenResponse.java
@@ -0,0 +1,14 @@
+package com.sl.shopingCart.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommenResponse {
+	private String msg ;
+	private int code;
+
+}
